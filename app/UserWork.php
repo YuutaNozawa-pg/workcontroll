114d8116ac3f844e44a3aac54609c82cb261431e
YutaNozawa <yuta.nozawa.pg@gmail.com>
2019-12-01 22:49:31 +0900

    [update] $fillableとgetUserWorks()の追加
    $fillable...同時に操作出来るフィールドの種類
    getUserWorks()はそのままテーブルに入っているレコードを全部返すメソッド

--- app/UserWork.php
+++ app/UserWork.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserWork extends Model
 {
+    protected $table = 'user_works';
     //
+    public $fillable = [
+        'user_id',
+        'work_time',
+        'start_time',
+        'end_time',
+        'over_time',
+        'break_time'
+    ];
+
+    public function getUserWorks() {
+        return $this->get();
+    }
 }
